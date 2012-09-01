ec828b4a8cb57a4c8908ec7683979169adff234b
James Halliday <mail@substack.net>
2012-08-31 21:33:03 -0700

    attach on all function prototypes

--- index.js
+++ index.js
@@ -8,16 +8,9 @@ module.exports = function (obj, fn) {
     C.prototype = Object.getPrototypeOf(obj);
     f.__proto__ = new C;
     
-    if (f.__proto__.call === undefined) {
-        f.__proto__.call = function (inst) {
-            return fn.apply(inst, [].slice.call(arguments, 1));
-        };
-    }
-    if (f.__proto__.apply === undefined) {
-        f.__proto__.apply = function (inst, args) {
-            return fn.apply(inst, args);
-        };
-    }
+    Object.getOwnPropertyNames(Function.prototype).forEach(function (key) {
+        f.__proto__[key] = Function.prototype[key];
+    });
     
     Object.getOwnPropertyNames(obj).forEach(function (key) {
         f[key] = obj[key];
