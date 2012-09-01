f4ab321239ecc2c89aa6b147685e181788b0e6f8
James Halliday <mail@substack.net>
2012-08-31 21:26:08 -0700

    also check .bind

--- test/foo.js
+++ test/foo.js
@@ -16,7 +16,7 @@ Foo.prototype.beep = function () {
 };
 
 test('funstantiate foo', function (t) {
-    t.plan(6);
+    t.plan(8);
     
     var obj = new Foo(4);
     var fobj = funstance(obj, function (n) {
@@ -31,6 +31,9 @@ test('funstantiate foo', function (t) {
     
     t.equal(fobj.call(null, 25), 100);
     t.equal(fobj.apply(null, [64]), 256);
+    t.equal(fobj.bind(null, 0.25)(), 1);
+    
     t.ok(!obj.call);
     t.ok(!obj.apply);
+    t.ok(!obj.bind);
 });
